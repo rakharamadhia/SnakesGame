45d6f9315742d1d3cb2e8e5c9b1cd0e49c8ac510
rakharamadhia <rakharamadhia@gmail.com>
2023-03-17 18:22:10 +0700

    add snake movement

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
-from turtle import Turtle, Screen
+from turtle import Screen
+from snake import Snake
+import time
 
 screen = Screen()
 screen.setup(600, 600)
 screen.bgcolor("black")
 screen.title("Snakes")
-starting_positions = [(0, 0), (-20, 0), [-40, 0]]
-segments = []
+screen.tracer(0)
 
-for position in starting_positions:
-    new_segment = Turtle(shape="square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
+snake = Snake()
+# food = Food()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 game_is_on = True
 while game_is_on:
-    for seg in segments:
-        seg.forward(20)
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
 
 
 screen.exitonclick()
